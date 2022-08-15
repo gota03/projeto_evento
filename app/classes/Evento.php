16a3a1d1579619f758b2cf06899164ab5395c71a
Mateus Matos <mateus.m.oliveira2303@gmail.com>
2022-08-15 20:38:47 -0300

    Organizando MVC

--- app/classes/Evento.php
+++ app/classes/Evento.php
@@ -4,15 +4,49 @@ class Evento{
     public $nomeEvento;
     public $dataEvento;
     public $banner;
-    public $mensagem = []; // ESSE ATRIBUTO IRA ARMAZENAR AS INFORMAÇÕES DE ERRO E SUCESS
+    public $mensagem = []; // ESSE ATRIBUTO IRA ARMAZENAR AS INFORMAÇÕES DE ERRO E SUCCESS
 
     //  CRIANDO METODOS(COMPORTAMENTOS)
+
+    public function inicio($campos, $arquivo){
+        // Verificar se os campos estão em branco
+        if($this->recebeDados($campos)){
+            if($this->validaData($campos["dataEvento"])){
+                if($this->recebeArquivo($arquivo)){
+                    $this->mensagem = [
+                        "status" => true,
+                        "msg" => "Evento cadastrado com sucesso"
+                    ];
+                }
+                else{
+                    $this->mensagem = [
+                        "status" => false,
+                        "msg" => "Você precisa enviar uma imagem com o formato jpg ou png"
+                    ];
+                }
+            }
+               else{
+                $this->mensagem = [
+                    "status" => false,
+                    "msg" => "Data do evento é anterior ao dia atual"
+                ];
+            }   
+        }
+        else {
+            $this->mensagem = [
+                "status" => false,
+                "msg" => "Os campos não podem ficar em branco"
+            ]; 
+        }
+        return $this->mensagem;
+    }
+
      public function validaData($data){
         $dataEvento = new DateTime($data); // ESTA CLASSE PRECISA DE UMA DATA NO PADRÃO AMERICANO
         $dataAtual = new DateTime("now"); // ESTAMOS PEGANDO A DATA ATUAL
-        echo $dataEvento->format("d/m/Y"); // MOSTRANDO A DATA NO PADRÃO BRASILEIRO
-        echo "<br> A data de hoje é: ";
-        print_r($dataAtual);
+        //echo $dataEvento->format("d/m/Y"); // MOSTRANDO A DATA NO PADRÃO BRASILEIRO
+        //echo "<br> A data de hoje é: ";
+        //print_r($dataAtual);
         if($dataEvento>$dataAtual){
             //echo "<p>Data do evento cadastrada com sucesso!</p>";
             return true;
@@ -42,18 +76,19 @@ class Evento{
         $this->banner["name"];
 
         if(empty($this->banner["name"])){
-            echo "<br> Arquivo vazio";
+            return false;
+            //echo "<br> Arquivo vazio";
         }
         else{
-            echo "<br> Arquivo aceito";
+            //echo "<br> Arquivo aceito";
             $infoArquivo = pathinfo($this->banner["name"]); // PATHINFO RETORNA UM ARRAY COM INFORMAÇÕES MAIS DETALHADAS DO ARQUIVO  
-            echo "<br>";
-            echo "<pre>";
-            print_r($infoArquivo);
-            echo "</pre>";
+            //echo "<br>";
+            //echo "<pre>";
+            //print_r($infoArquivo);
+            //echo "</pre>";
 
         if($infoArquivo["extension"] == "jpg" || $infoArquivo["extension"] == "png"){
-            echo "<br> Formato de arquivo aceito";
+            //echo "<br> Formato de arquivo aceito";
         //  COPIANDO IMAGEM PARA O SERVIDOR
             $pasta = "../imagens/";
         // IREMOS VERIFICAR SE A PASTA EXISTE OU NÃO
@@ -62,17 +97,22 @@ class Evento{
         }
             $caminhoFinal = $pasta.$this->banner["name"];
             move_uploaded_file($this->banner["tmp_name"], $caminhoFinal);
-            echo "<img src='{$caminhoFinal}' width='200px' height='200px'>";
+            //echo "<img src='{$caminhoFinal}' width='200px' height='200px'>";
+            return true;
         }
         else{
-            echo "<br> Formato de arquivo inválido";
+            //echo "<br> Formato de arquivo inválido";
+            return false;
         }
         }
     }
 }
 
 // INSTANCIANDO UM OBJETO
+/*
 $meuEvento = new Evento();
 print_r($meuEvento);
 echo "<hr>";
 $meuEvento->recebeDados($_POST);
+$meuEvento->inicio($_POST, $_FILES);
+*/
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+require_once("app/view/CadastroView.html");
+require_once("app/view/CadastroView.css");

e74c908fe2cd24c95c2bbbf9f859435458b3c668
Mateus Matos <mateus.m.oliveira2303@gmail.com>
2022-08-18 21:01:16 -0300

    iniciado model

---
+++ app/model/Conexao.php
@@ -0,0 +1,12 @@
+<?php
+class Conexao{
+    private static $instancia;
+    
+    public static function getConexao(){
+        if(!isset(self::$instancia)){ // ESTOU TESTANDO QUANDO O ATRIBUTO NÃO EXISTIR
+            self::$instancia = new PDO("
+                mysql: host=localhost;dbname=projeto_evento", "root", "");
+
+        }
+    }
+}

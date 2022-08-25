18d732dfbb86ce56602e2a35d665f12972b66b5c
Mateus Matos <mateus.m.oliveira2303@gmail.com>
2022-08-24 21:00:07 -0300

    finalizado a interface de visualizar evento

--- app/includes/cabecalho.php
+++ app/includes/cabecalho.php
@@ -11,6 +11,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Eventos</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
 
 </head>
 
--- app/view/VisualizarEventoView.php
+++ app/view/VisualizarEventoView.php
@@ -1,24 +1,60 @@
 <?php
-include_once("../includes/cabecalho.php");
+    include_once("../includes/cabecalho.php");
+    require_once("../model/EventoDAO.php");
+
+    $meuEventoDAO = new EventoDAO();
 ?>
 
  <main class="container-fluid mt-5">
 
-    <section class="d-flex justify-content-between">
+    <section class="d-flex justify-content-between border border-dark rounded-pill p-2">
 
-        <h3 class="fw-bold">Gerenciamento de Eventos</h3>
+        <h3 class="fw-bold ms-3 my-auto">Gerenciamento de Eventos</h3>
+
         <a href="CadastroView.php" class="btn btn-primary me-4">Criar Evento</a>
 
     </section>
 
     <section class="row row-cols-sm-1 row-cols-md-2 row-cols-lg-3 g-4">
 
-    
+        <section>
+        
+            <div class="card mt-5">
+
+                <img src="" alt="" class="card-img-top">
+
+                <div class="card-body">
+
+                    <h5 class="card-title text-center">Titulo do evento</h5>
+
+                    <p class="card-text fw-semibold mt-3 ms-2">O evento ocorrerá na data</p>
+
+                </div>
+
+                <div class="card-footer">
+
+                    <form action="" method="post" class="d-flex justify-content-around">
+                    
+                        <button type="submit" class="btn btn-info text-light col-5 d-flex justify-content-between align-items-center">
+                        EDITAR <span class="material-symbols-outlined">edit</span>
+                        </button>
+
+                        <button type="submit" class="btn btn-danger text-light col-5 d-flex justify-content-between align-items-center">
+                        EXCLUIR <span class="material-symbols-outlined ms-2">delete</span>
+                        </button>
+                    
+                    </form>
+                
+                </div>
+            
+            </div>
+
+        </section>
     
     </section>
 
  </main>
 
- <?php
+<?php
     include_once("../includes/rodape.php");
 ?>
